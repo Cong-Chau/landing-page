7da25394117d8f0f5fe9f76a4f947975149a4ca8
Cong Chau <congchau206#gmail.com>
2025-07-30 18:09:03 +0700

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  turbo: false,
-  devIndicators: {
-    buildActivity: false,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
